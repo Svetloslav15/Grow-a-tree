242bd75f4a50c91bb8f826461601b49995d405f5
Svetloslav15 <sn_novoselski@abv.bg>
2020-10-11 21:41:50 +0300

    add project structure

---
+++ src/FrontEnd/src/components/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {h1} from './HomePage.module.scss';
+
+const HomePage = () => {
+  return (
+      <h1 className={h1}>Home Page</h1>
+  );
+};
+
+export default HomePage;
