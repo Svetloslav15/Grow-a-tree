5dbf8278510a74f531f238901d47db8756c3dc9e
Svetloslav15 <sn_novoselski@abv.bg>
2020-10-21 23:00:45 +0300

    setup react-router-dom

--- src/FrontEnd/src/components/common/Navigation/NavItem/NavItem.jsx
+++ src/FrontEnd/src/components/common/Navigation/NavItem/NavItem.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import * as style from './NavItem.module.scss';
+import {Link} from 'react-router-dom';
 
 const NavItem = ({children, link}) => (
     <li className={`${style.wrapper} nav-item`}>
-        <a className={`${style.link} nav-link`} href={link}>{children}</a>
+        <Link className={`${style.link} nav-link`} to={link}>{children}</Link>
     </li>
 );
 
