219d32d7b9581accc5df2d6a66abf86a8657a2f7
Svetloslav15 <sn_novoselski@abv.bg>
2020-10-21 22:41:12 +0300

    change content in navigation

--- src/FrontEnd/src/components/common/Navigation/Navigation.jsx
+++ src/FrontEnd/src/components/common/Navigation/Navigation.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
 import NavItem from './NavItem/NavItem';
-import NavDropDownItem from './NavDropDownItem/NavDropDownItem';
 import NavCollapseButton from './NavCollapseButton/NavCollapseButton';
-import NavInputItem from './NavInputItem/NavInputItem';
 import * as style from './Navigation.module.scss';
+
+const Logo = require('../../../assets/logo.png');
 
 const Navigation = () => {
     return (
         <nav className={`${style.navigation} navbar navbar-expand-lg`}>
             <NavCollapseButton/>
+            <img src={Logo} className={style.logo} alt="Grow A Tree Logo"/>
             <div className="collapse navbar-collapse" id="basicExampleNav">
                 <ul className="navbar-nav mr-auto">
-                    <NavItem text={'text'} link={'#'}/>
-                    <NavItem text={'text1'} link={'#'}/>
-                    <NavItem text={'text2'} link={'#'}/>
-                    <NavItem text={'text3'} link={'#'}/>
-                    <NavDropDownItem/>
+                    <NavItem text={'Карта'} link={'#'}/>
+                    <NavItem text={'За платформата'} link={'#'}/>
+                    <NavItem text={'Класация'} link={'#'}/>
+                    <NavItem text={'Магазин'} link={'#'}/>
+                    <NavItem text={'Събития'} link={'#'}/>
+                    <NavItem text={'Форум'} link={'#'}/>
                 </ul>
-                <NavInputItem/>
             </div>
         </nav>
     )
