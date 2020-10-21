a7fd2e125676e128b2d5e6b204db795f5f4b3e21
Svetloslav15 <sn_novoselski@abv.bg>
2020-10-21 22:50:46 +0300

    fix navigation style

--- src/FrontEnd/src/components/common/Navigation/NavItem/NavItem.jsx
+++ src/FrontEnd/src/components/common/Navigation/NavItem/NavItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import * as style from './NavItem.module.scss';
 
-const NavItem = ({text, link}) => (
-    <li className='nav-item'>
-        <a className={`${style.link} nav-link`} href={link}>{text}</a>
+const NavItem = ({children, link}) => (
+    <li className={`${style.wrapper} nav-item`}>
+        <a className={`${style.link} nav-link`} href={link}>{children}</a>
     </li>
 );
 
--- src/FrontEnd/src/components/common/Navigation/Navigation.jsx
+++ src/FrontEnd/src/components/common/Navigation/Navigation.jsx
@@ -2,22 +2,26 @@ import React from 'react';
 import NavItem from './NavItem/NavItem';
 import NavCollapseButton from './NavCollapseButton/NavCollapseButton';
 import * as style from './Navigation.module.scss';
+import Button from '../../common/Button/Button';
 
 const Logo = require('../../../assets/logo.png');
 
 const Navigation = () => {
     return (
-        <nav className={`${style.navigation} navbar navbar-expand-lg`}>
+        <nav className={`${style.navigation} py-0 navbar navbar-expand-lg`}>
             <NavCollapseButton/>
             <img src={Logo} className={style.logo} alt="Grow A Tree Logo"/>
             <div className="collapse navbar-collapse" id="basicExampleNav">
                 <ul className="navbar-nav mr-auto">
-                    <NavItem text={'Карта'} link={'#'}/>
-                    <NavItem text={'За платформата'} link={'#'}/>
-                    <NavItem text={'Класация'} link={'#'}/>
-                    <NavItem text={'Магазин'} link={'#'}/>
-                    <NavItem text={'Събития'} link={'#'}/>
-                    <NavItem text={'Форум'} link={'#'}/>
+                    <NavItem link={'#'}>Карта</NavItem>
+                    <NavItem link={'#'}>За платформата</NavItem>
+                    <NavItem link={'#'}>Класация</NavItem>
+                    <NavItem link={'#'}>Магазин</NavItem>
+                    <NavItem link={'#'}>Събития</NavItem>
+                    <NavItem link={'#'}>Форум</NavItem>
+                    <NavItem link={'/login'}>
+                        <Button type='GreenOutline'>Вход</Button>
+                    </NavItem>
                 </ul>
             </div>
         </nav>
