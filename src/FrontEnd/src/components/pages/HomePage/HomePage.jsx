358b586bbc2a5e0b11c967a9f2f8514822330cb0
Svetloslav15 <sn_novoselski@abv.bg>
2020-10-21 22:51:16 +0300

    remove unused button

--- src/FrontEnd/src/components/pages/HomePage/HomePage.jsx
+++ src/FrontEnd/src/components/pages/HomePage/HomePage.jsx
@@ -14,7 +14,6 @@ const HomePage = () => {
             <Button type='DarkOutline'>DarkOutline</Button>
             <Button type='Red'>Red</Button>
             <Button type='Yellow'>Yellow</Button>
-            <Button type='Green'>Alexandrina</Button>
         </div>
     );
 };
