e7533064e68b387c0dd4039cc166afa962a0e450
Svetloslav15 <sn_novoselski@abv.bg>
2020-10-22 22:10:33 +0300

    add shapes in bg

--- src/FrontEnd/src/components/pages/HomePage/HomePage.jsx
+++ src/FrontEnd/src/components/pages/HomePage/HomePage.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
-import {h1} from './HomePage.module.scss';
+import * as style from './HomePage.module.scss';
+
 import Button from '../../common/Button/Button'
 import Navigation from '../../common/Navigation/Navigation';
+
+const BgShape1 = require('../../../assets/bg-shape-1.png');
+const BgShape2 = require('../../../assets/bg-shape-2.png');
+const BgShape3 = require('../../../assets/bg-shape-3.png');
 
 const HomePage = () => {
     return (
         <div>
             <Navigation/>
-            <h1 className={h1}>Home Page</h1>
+            <img src={BgShape1} className={style.shape1} alt=""/>
+            <img src={BgShape2} className={style.shape2} alt=""/>
+            <img src={BgShape3} className={style.shape3} alt=""/>
+            <h1 className={style.h1}>Home Page</h1>
             <Button type='Green'>Green</Button>
             <Button type='GreenOutline'>GreenOutline</Button>
             <Button type='Dark'>Dark</Button>
