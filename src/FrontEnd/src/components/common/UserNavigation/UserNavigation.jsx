f39df46e6f1aa25eb07aae0a41156b9a575db8d9
Svetloslav15 <sn_novoselski@abv.bg>
2020-11-10 18:31:26 +0200

    add close icon

--- src/FrontEnd/src/components/common/UserNavigation/UserNavigation.jsx
+++ src/FrontEnd/src/components/common/UserNavigation/UserNavigation.jsx
@@ -4,13 +4,14 @@ import Item from './Item/Item';
 
 import * as style from './UserNavigation.module.scss';
 const UserImage = require('../../../assets/user-profile.png');
+const UpArrow = require('../../../assets/up-arrow.png');
 
 const UserNavigation = ({props}) => (
     <div className={style.wrapper}>
         <div className={`mx-0 row ${style.userSection}`}>
             <img className={style.userProfileImage} src={UserImage} alt=""/>
             <div className=''>
-                <p>@Svetloslav</p>
+                <p className={style.username}>@Svetloslav</p>
                 <p>(Svetloslav Novoselski)</p>
             </div>
         </div>
@@ -24,6 +25,9 @@ const UserNavigation = ({props}) => (
             <Item link='/' text='Моят профил' icon='user'/>
             <Item link='/' text='Моят профил' icon='user'/>
             <Item link='/' text='Моят профил' icon='user'/>
+            <div className='col-md-12 text-center'>
+                <img className={style.collapseIcon} src={UpArrow} alt="Close Icon"/>
+            </div>
         </div>
     </div>
 );
