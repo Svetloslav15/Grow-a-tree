72be0f6704f33e264d57b237975982846c15d802
Svetloslav15 <sn_novoselski@abv.bg>
2020-11-10 18:23:28 +0200

    style whole sidebar navigation

--- src/FrontEnd/src/components/common/UserNavigation/UserNavigation.jsx
+++ src/FrontEnd/src/components/common/UserNavigation/UserNavigation.jsx
@@ -3,25 +3,28 @@ import {Link} from 'react-router-dom';
 import Item from './Item/Item';
 
 import * as style from './UserNavigation.module.scss';
+const UserImage = require('../../../assets/user-profile.png');
 
 const UserNavigation = ({props}) => (
     <div className={style.wrapper}>
-        <div className={`row ${style.userSection}`}>
-            <img className={style.userProfileImage} src="" alt=""/>
+        <div className={`mx-0 row ${style.userSection}`}>
+            <img className={style.userProfileImage} src={UserImage} alt=""/>
             <div className=''>
                 <p>@Svetloslav</p>
                 <p>(Svetloslav Novoselski)</p>
             </div>
         </div>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
-        <Item link='/' text='Моят профил' icon='user'/>
+        <div>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+            <Item link='/' text='Моят профил' icon='user'/>
+        </div>
     </div>
 );
 
