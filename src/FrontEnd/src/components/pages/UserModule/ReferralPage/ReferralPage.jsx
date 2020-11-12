cdae2617b18b3435817fdde062fc8a741490af05
Svetloslav15 <sn_novoselski@abv.bg>
2020-11-12 16:58:42 +0200

    set actual link

--- src/FrontEnd/src/components/pages/UserModule/ReferralPage/ReferralPage.jsx
+++ src/FrontEnd/src/components/pages/UserModule/ReferralPage/ReferralPage.jsx
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useSelector} from 'react-redux';
 import Layout from '../Layout/Layout';
 import * as style from './ReferralPage.module.scss';
 import QRCode from "react-qr-code";
 import InputField from "../../../common/InputField/InputField";
 import Button from "../../../common/Button/Button";
+import AlertService from '../../../../services/alertService';
 
 const ReferralPage = () => {
+    const currUser = useSelector(state => state.auth);
+    const [referralLink, setReferralLink] = useState('');
+
+    useEffect(() => {
+        setReferralLink(`${process.env.REACT_APP_BASE_URL}/auth/register?referral=${currUser.id}`)
+    }, [currUser]);
+
+    const copyToClipboard = () => {
+        navigator.clipboard.writeText(referralLink);
+        AlertService.success('Копирано');
+    };
+
     return (
         <Layout>
             <div className='p-5 col-md-5'>
                 <p className={style.title}># Покани приятел и получи бонус точки </p>
-                <QRCode value='https://github.com/Svetloslav15?tab=repositories'/>
+                <QRCode value={referralLink}/>
                 <div className='pt-2'>
                     <InputField
                         type='text'
-                        label='Линк за покана'
+                        label=''
                         id='referral-link'
-                        value='https://github.com/Svetloslav15?tab=repositories'
+                        value={referralLink}
                         disabled={true}
                     />
                     <div className={style.buttonsSection}>
                         <Button type='DarkOutline'>Сподели</Button>
-                        <Button type='Dark'>Копирай</Button>
+                        <Button type='Dark' onClick={copyToClipboard}>Копирай</Button>
                     </div>
                 </div>
             </div>
