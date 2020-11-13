70643b7646ef06a7d2e10f92d7bfef6707bc5fc0
Svetloslav15 <svetli.novoselski@gmail.com>
2020-11-13 23:02:52 +0200

    add map for add tree

--- src/FrontEnd/src/components/common/DropdownField/DropdownField.jsx
+++ src/FrontEnd/src/components/common/DropdownField/DropdownField.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const DropdownField = ({width, onChange, values}) => (
-    <select className={`mx-auto col-md-${width}`} onSelect={onChange}>
+const DropdownField = ({width, onChange, values, label, id}) => (
+    <select id={id}
+            className={`mx-auto mb-3 col-md-${width} custom-select`}
+            onChange={onChange}
+            defaultValue={label}>
+        <option value={label}>{label}</option>
         {
-            values.map(x => <option value={x}>{x}</option>)
+            values.map((x, i) => <option key={i} value={x}>{x}</option>)
         }
     </select>
 );
--- src/FrontEnd/src/components/pages/TreeModule/AddTreePage/AddTreePage.jsx
+++ src/FrontEnd/src/components/pages/TreeModule/AddTreePage/AddTreePage.jsx
@@ -1,14 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+
 import * as style from './AddTreePage.module.scss';
 import Layout from '../../../common/Layout/Layout';
 import InputField from '../../../common/InputField/InputField';
+import DropdownField from "../../../common/DropdownField/DropdownField";
+import Map from "../../../common/Map/Map";
 
-const AddTreePage = ({}) => (
-    <Layout>
-        <h2 className={style.title}># Добави дърво</h2>
-        <div>
-        </div>
-        <div></div>
-    </Layout>
-);
+const AddTreePage = ({}) => {
+    const [data, setData] = useState({});
+
+    const handleChange = (event) => {
+        data[event.target.id] = event.target.value;
+        setData(data);
+    };
+
+    const handleCoordinates = (latitude, longitute) => {
+        //TODO
+    };
+
+    return (
+        <Layout>
+            <div className='offset-md-1 col-md-6 p-5'>
+                <h2 className={style.title}># Добави дърво</h2>
+                <div className='col-md-12 row'>
+                    <div className='col-md-10'>
+                        <InputField type='email'
+                                    label={'Прякор'}
+                                    id='nickname'
+                                    width={12}
+                                    onChange={handleChange}/>
+                        <DropdownField width={12}
+                                       id='type'
+                                       label='Вид'
+                                       values={['test', 'test1', 'test2']}
+                                       onChange={handleChange}/>
+                        <DropdownField width={12}
+                                       id='category'
+                                       label='Категория'
+                                       values={['test', 'test1', 'test2']}
+                                       onChange={handleChange}/>
+                    </div>
+                    <div className={style.mapContainer}>
+                        <Map handleCoordinates={handleCoordinates}/>
+                    </div>
+                </div>
+            </div>
+        </Layout>
+    );
+};
+
 export default AddTreePage;
