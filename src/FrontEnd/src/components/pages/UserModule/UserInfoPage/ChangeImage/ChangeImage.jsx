28f7d5592e0a504e8b9dcd4ce5b3d0834c95b375
Svetloslav15 <svetli.novoselski@gmail.com>
2020-11-16 22:13:53 +0200

    add file input with styles

--- src/FrontEnd/src/components/pages/UserModule/UserInfoPage/ChangeImage/ChangeImage.jsx
+++ src/FrontEnd/src/components/pages/UserModule/UserInfoPage/ChangeImage/ChangeImage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Button from "../../../../common/Button/Button";
 import FileInput from "../../../../common/FileInput/FileInput";
 
-const ChangeImage = () => {
+const ChangeImage = ({onSubmit}) => {
     const handleFilesUpload = () => {
 
     };
@@ -11,19 +11,19 @@ const ChangeImage = () => {
         <>
             <div className='text-center'>
                 <Button type='DarkOutline'>
-                    <div className="row" data-toggle="modal" data-target="#basicExampleModal">
+                    <div className="row" data-toggle="modal" data-target="#profilePictureInputContainer">
                         <i className="fas fa-images mr-2"/>
                         Промени
                     </div>
                 </Button>
 
             </div>
-            <div className="modal fade" id="basicExampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+            <div className="modal fade" id="profilePictureInputContainer"  role="dialog" aria-labelledby="profilePictureInputContainer"
                  aria-hidden="true">
                 <div className="modal-dialog" role="document">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <h5 className="modal-title" id="profilePictureInputContainer">Добави снимка</h5>
                             <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
@@ -32,8 +32,10 @@ const ChangeImage = () => {
                             <FileInput onChange={handleFilesUpload}/>
                         </div>
                         <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Затвори</button>
-                            <button type="button" className="btn btn-primary">Промени</button>
+                            <Button type='Red'>
+                                <div aria-label="Close" data-dismiss="modal">Затвори</div>
+                            </Button>
+                            <Button type='Green' onClick={onSubmit}>Промени</Button>
                         </div>
                     </div>
                 </div>
