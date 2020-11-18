e53d0b23b638fa087e7fe44b74c3e8a7baeae6f3
Svetloslav15 <svetli.novoselski@gmail.com>
2020-11-18 22:09:40 +0200

    create image service and method to read data from url

---
+++ src/BackEnd/src/Common/Services/ImageService.cs
@@ -0,0 +1,30 @@
+﻿namespace Common.Services
+{
+    using System;
+    using System.IO;
+    using System.Net;
+    using Common.Interfaces;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Http.Internal;
+
+    /// <summary>
+    /// Service that process image helper operations
+    /// </summary>
+    public class ImageService : IImageService
+    {
+        /// <summary>
+        /// Read image data from url and convert to IFormFile
+        /// </summary>
+        /// <param name="url">Url parameter</param>
+        /// <returns>Image in IFormFile</returns>
+        public IFormFile ReadImageFromUrl(string url)
+        {
+            using (var webClient = new WebClient())
+            {
+                byte[] imageBytes = webClient.DownloadData(url);
+                var stream = new MemoryStream(imageBytes);
+                return new FormFile(stream, 0, imageBytes.Length, "name", Guid.NewGuid().ToString());
+            }
+        }
+    }
+}
