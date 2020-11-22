a98f58b31f3df350b5cf0a010c6dea96cf42dbda
Svetloslav15 <svetli.novoselski@gmail.com>
2020-11-22 12:58:50 +0200

    style carousel

--- src/FrontEnd/src/components/pages/HomeModule/HomePage/Carousel/Carousel.jsx
+++ src/FrontEnd/src/components/pages/HomeModule/HomePage/Carousel/Carousel.jsx
@@ -1,24 +1,20 @@
 import React, {useEffect} from 'react';
 import * as style from './Carousel.module.scss';
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 const responsive = {
-    superLargeDesktop: {
-        // the naming can be any, depends on you.
-        breakpoint: { max: 4000, min: 3000 },
-        items: 5
-    },
     desktop: {
         breakpoint: { max: 3000, min: 1024 },
-        items: 3
+        items: 5,
     },
     tablet: {
         breakpoint: { max: 1024, min: 464 },
-        items: 2
+        items: 2,
     },
     mobile: {
         breakpoint: { max: 464, min: 0 },
-        items: 1
+        items: 1,
     }
 };
 const CarouselComponent = () => {
