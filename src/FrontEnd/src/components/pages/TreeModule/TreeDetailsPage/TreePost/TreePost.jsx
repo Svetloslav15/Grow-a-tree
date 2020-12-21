377ded902a1d05f82bd26b776a85beca78f6ae02
Svetloslav15 <svetli.novoselski@gmail.com>
2020-12-22 00:33:45 +0200

    list posts

--- src/FrontEnd/src/components/pages/TreeModule/TreeDetailsPage/TreePost/TreePost.jsx
+++ src/FrontEnd/src/components/pages/TreeModule/TreeDetailsPage/TreePost/TreePost.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
+import parse from 'html-react-parser';
+
 import './TreePost.scss';
 
-const TreePost = ({data}) => (
-    <div className='post'>
-        <div className='post__user-section'>
-            <img className='post__user-section__avatar' src={data.userProfilePictureUrl} alt={data.userUserName}/>
-            <p className='post__user-section__username' >{data.userUserName}</p>
+const TreePost = ({data}) => {
+    return (
+        <div className='post'>
+            <div className='post__user-section'>
+                <img className='post__user-section__avatar' src={data.userProfilePictureUrl} alt={data.userUserName}/>
+                <p className='post__user-section__username' >{data.userUserName}</p>
+            </div>
+            <div className='post__content'>
+                {parse(data.content)}
+            </div>
         </div>
-        <div className='post__content' >
-            {data.content}
-        </div>
-    </div>
-);
+    );
+}
 
 export default TreePost;
