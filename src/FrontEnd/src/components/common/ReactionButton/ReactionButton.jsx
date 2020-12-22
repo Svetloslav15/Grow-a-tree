a74039f68e51a1419c3691aa68340f9f245026a8
Svetloslav15 <svetli.novoselski@gmail.com>
2020-12-22 23:35:54 +0200

    create reaction button component

---
+++ src/FrontEnd/src/components/common/ReactionButton/ReactionButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ReactionButton.scss';
+
+const ReactionButton = () => {
+    return (
+        <div>
+
+        </div>
+    )
+};
+
+export default ReactionButton;
