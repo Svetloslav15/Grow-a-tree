67e531c90a8920fb5c2b10edd7233c897f2d006f
Svetloslav15 <svetli.novoselski@gmail.com>
2021-01-10 19:53:25 +0200

    add modal for adding report

--- src/FrontEnd/src/components/pages/TreeModule/TreeDetailsPage/ReportModal/ReportModal.jsx
+++ src/FrontEnd/src/components/pages/TreeModule/TreeDetailsPage/ReportModal/ReportModal.jsx
@@ -1,11 +1,18 @@
 import React, {useEffect, useState} from 'react';
 import styles from './Report.module.scss';
+import InputField from '../../../../common/InputField/InputField';
+import DropdownField from "../../../../common/DropdownField/DropdownField";
+import FileInput from "../../../../common/FileInput/FileInput";
+import Button from "../../../../common/Button/Button";
 
 const BgShapeTwo = require('../../../../../assets/bg-shape-2.png');
 const BgShapeThree = require('../../../../../assets/bg-shape-3.png');
 const BgAltTag = 'Background Shape Image';
 
 const ReportModal = ({closeModal}) => {
+    const addReport = () => {
+
+    }
     return (
         <div className={styles.wrapper}>
             <div className={styles.wrapper__bgOverlay} onClick={closeModal}></div>
@@ -13,7 +20,15 @@ const ReportModal = ({closeModal}) => {
                 <img className={styles.bgShapeImage} src={BgShapeTwo} alt={BgAltTag}/>
                 <img className={styles.bgShapeImage} src={BgShapeThree} alt={BgAltTag}/>
                 <i className={`fas fa-times ${styles.closeButton}`} onClick={closeModal}/>
-                <p className={styles.wrapper__modal__title}>Добави доклад</p>
+                <p className={styles.wrapper__modal__title}>Докладвай проблем</p>
+                <div className={styles.wrapper__modal__form}>
+                    <InputField label='Описание'/>
+                    <DropdownField defaultValue='Липсващо' values={['Счупено', 'Увехнало']}/>
+                    <FileInput/>
+                    <div className='w-100 text-center'>
+                        <Button type='Dark' onClick={addReport} className='mx-auto'>Добави</Button>
+                    </div>
+                </div>
             </div>
         </div>
     )
