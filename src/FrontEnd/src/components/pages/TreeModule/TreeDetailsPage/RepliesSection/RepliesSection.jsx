c2507b07206f24e9694a41cf780c3095124bd84f
Svetloslav15 <svetli.novoselski@gmail.com>
2021-01-11 00:05:37 +0200

    setup logic for creating reply

--- src/FrontEnd/src/components/pages/TreeModule/TreeDetailsPage/RepliesSection/RepliesSection.jsx
+++ src/FrontEnd/src/components/pages/TreeModule/TreeDetailsPage/RepliesSection/RepliesSection.jsx
@@ -1,11 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import styles from './RepliesSection.module.scss';
 import ReactionButton from "../../../../common/ReactionButton/ReactionButton";
-
-const DefaultUserImage = require('../../../../../assets/user-profile.png');
+import Button from "../../../../common/Button/Button";
+import {Editor} from "@tinymce/tinymce-react";
+import parse from 'html-react-parser';
 
 const RepliesSection = ({replies, postId}) => {
     const [repliesData, setReplies] = useState(replies);
+    const [isAddCommentInputOpen, setIsAddCommentInputOpen] = useState(false);
+    const [editorKey, setEditorKey] = useState(4);
+    const [currentComment, setCurrentComment] = useState('');
 
     useEffect(() => {
         setReplies(replies.filter(x => x.treePostId === postId));
@@ -15,15 +19,51 @@ const RepliesSection = ({replies, postId}) => {
 
     }
 
+    const openAddCommentInput = () => {
+        if (isAddCommentInputOpen) {
+            addComment();
+            setIsAddCommentInputOpen(false);
+        } else {
+            setIsAddCommentInputOpen(true);
+        }
+    }
+
+    const handleEditorChange = (data) => {
+        setCurrentComment(data);
+    }
+
+    const addComment = () => {
+        console.log(currentComment);
+    }
+
     return (
         <div className={styles.wrapper}>
             <ul className={styles.wrapper__items}>
+                <Button type='Green' onClick={openAddCommentInput}>Коментирай</Button>
+                {isAddCommentInputOpen && <Editor
+                    key={editorKey}
+                    apiKey={process.env.REACT_APP_TINYMCE_KEY}
+                    init={{
+                        height: 200,
+                        menubar: false,
+                        plugins: [
+                            'advlist autolink lists link image charmap print preview anchor',
+                            'searchreplace visualblocks code fullscreen',
+                            'insertdatetime media table paste code help wordcount imagetools'
+                        ],
+                        toolbar:
+                            'formatselect  | image link | bold italic backcolor | \
+                            alignleft aligncenter alignright alignjustify',
+                        file_picker_types: 'file image',
+                    }}
+                    onEditorChange={handleEditorChange}
+                />}
                 {repliesData.map(x => <li className={styles.wrapper__items__item}>
                     <div className={styles.wrapper__items__item__userWrapper}>
                         <img src={x.userProfilePictureUrl} alt={x.userUserName}/>
                         <span>{x.userUserName}</span>
                     </div>
-                    <p className={styles.wrapper__items__item__content}>{x.content}</p>
+                    <p className={styles.wrapper__items__item__content}>{parse(x.content)}</p>
                     <ReactionButton hasBorder={false}
                                     reactionsVisible={true}
                                     hasCustomButton={true}
