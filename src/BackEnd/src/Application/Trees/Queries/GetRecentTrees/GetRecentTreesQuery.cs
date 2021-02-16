c909e5da8db5fef622852b7521c9956679333248
Svetloslav15 <svetli.novoselski@gmail.com>
2021-02-16 21:28:33 +0200

    add handler for recent trees

---
+++ src/BackEnd/src/Application/Trees/Queries/GetRecentTrees/GetRecentTreesQuery.cs
@@ -0,0 +1,10 @@
+﻿namespace GrowATree.Application.Trees.Queries.GetRecentTrees
+{
+    using GrowATree.Application.Models.Common.Models;
+    using GrowATree.Application.Models.Trees;
+    using MediatR;
+
+    public class GetRecentTreesQuery : PagedQuery, IRequest<TreeListModel>
+    {
+    }
+}
---
+++ src/BackEnd/src/Application/Trees/Queries/GetRecentTrees/GetRecentTreesQueryHandler.cs
@@ -0,0 +1,56 @@
+﻿namespace GrowATree.Application.Trees.Queries.GetRecentTrees
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using GrowATree.Application.Common.Interfaces;
+    using GrowATree.Application.Models.Common.Models;
+    using GrowATree.Application.Models.Trees;
+    using MediatR;
+
+    class GetRecentTreesQueryHandler : IRequestHandler<GetRecentTreesQuery, TreeListModel>
+    {
+        private readonly IApplicationDbContext context;
+        private readonly IMapper mapper;
+
+        public GetRecentTreesQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public async Task<TreeListModel> Handle(GetRecentTreesQuery request, CancellationToken cancellationToken)
+        {
+            var list = await this.context.Trees
+                .OrderByDescending(tree => tree.PlantedOn)
+                .Skip(request.PerPage * (request.Page - 1))
+                .Take(request.PerPage)
+                .ProjectTo<TreeModel>(this.mapper.ConfigurationProvider)
+                .ToListAsync();
+
+            var totalTrees = list.Count;
+            var meta = new Pagination
+            {
+                CurrentPage = request.Page,
+                PerPage = request.PerPage,
+                TotalItems = totalTrees,
+                TotalPages = Convert.ToInt32(Math.Ceiling(totalTrees / Convert.ToDouble(request.PerPage))),
+            };
+
+            var result = new TreeListModel
+            {
+                Data = list,
+                Meta = new PaginationMeta
+                {
+                    Pagination = meta,
+                },
+            };
+
+            return result;
+        }
+    }
+}
