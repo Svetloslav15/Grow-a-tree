4e0f09f3d1e0b28bab2a630685915d97e0810ab9
Svetloslav Novoselski <svetli.novoselski@gmail.com>
2021-03-17 23:19:48 +0200

    add logs

--- src/BackEnd/src/Application/Auth/Commands/Register/RegisterCommandHandler.cs
+++ src/BackEnd/src/Application/Auth/Commands/Register/RegisterCommandHandler.cs
@@ -9,6 +9,7 @@
     using GrowATree.Domain.Entities;
     using MediatR;
     using Microsoft.AspNetCore.Identity;
+    using Serilog;
 
     /// <summary>
     /// Hadler that implement register logic.
@@ -26,6 +27,7 @@
 
         public async Task<Result<bool>> Handle(RegisterCommand request, CancellationToken cancellationToken)
         {
+            Log.Logger.Error("Handler Beggining: " + request.Email);
             if (await this.userManager.FindByEmailAsync(request.Email) != null)
             {
                 return Result<bool>.Failure(ErrorMessages.EmailInUseErrorMessage);
@@ -57,6 +59,7 @@
 
             string token = await this.userManager.GenerateEmailConfirmationTokenAsync(user);
             token = HttpUtility.UrlEncode(token);
+            Log.Logger.Error("Handler After creatting user: " + user.Email);
 
             string confirmationLink = Constants.ConfirmEmailLink + "?token=" + token;
             bool result = await this.emailSender.SendEmail(user, confirmationLink, "Grow A Tree: Confirm email");
